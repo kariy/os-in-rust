a90adab6c2f587f3dbadc49a0f370cc2490752fc
Ammar Arif <evergreenkary@gmail.com>
2024-07-15 20:49:31 -0400

    a minimal rust kernel

---
+++ README.md
@@ -0,0 +1,3 @@
+# Operating System in Rust
+
+Based on the blog series [Writing an OS in Rust](https://os.phil-opp.com/).
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-#![no_std]
-#![no_main]
+#![no_std] // don't link the rust standard library
+#![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
 
@@ -14,9 +14,20 @@ fn panic(_info: &PanicInfo) -> ! {
 /// _ZN3blog_os4_start7hb173fedf945531caE symbol to give every function a unique name.
 ///
 /// [mangling]: https://en.wikipedia.org/wiki/Name_mangling
+///
+/// This function is the entry point, since the linker looks for a function
+/// named `_start` by default.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by defaul
+    // cast the integer into a raw mutable pointer
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in b"Hello World!".iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte; // ASCII byte
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // color byte (light cyan)
+        }
+    }
+
     loop {}
 }
